0060bf3665e13896f5ad64664d469585b3ede53d
Nicu2002 <nicu.mercurial@bk.ru>
2022-03-10 11:40:01 +0200

    lab3 procces

---
+++ FourWheels/Scripts/porche-slider.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const sliderWrap = document.getElementById(".slider-carrousel");
+const sliderImgs = document.querySelectorAll("por")
+
+sliderImgs.forEach(img => {
+    console.log(img);
+});
