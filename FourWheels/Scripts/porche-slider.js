013da602af8a1a195db7756af15b0645a8f6f00f
Nicu2002 <nicu.mercurial@bk.ru>
2022-03-12 15:57:37 +0200

    finished porche-slider

--- FourWheels/Scripts/porche-slider.js
+++ FourWheels/Scripts/porche-slider.js
@@ -1,8 +1,49 @@
 "use strict";
 
-const sliderWrap = document.getElementById(".slider-carrousel");
-const sliderImgs = document.querySelectorAll("por")
+const sliderWrap = document.querySelector(".slider-carrousel");
+const sliderImgs = sliderWrap.querySelectorAll("img");
 
-sliderImgs.forEach(img => {
-    console.log(img);
+const leftBtn = document.querySelector(".slider-arrows img");
+const rightBtn = document.querySelector(".slider-arrows img:last-child");
+
+const imgLengh = window.getComputedStyle(sliderImgs[0]).width.slice(0, -2);
+
+const sliderPoints = document.querySelectorAll("#slider-points div");
+
+let counter = 0;
+
+sliderPoints[counter].style.opacity = "100%";
+
+rightBtn.addEventListener("click", () => {
+    if (counter != sliderImgs.length - 1) {
+        counter++;
+        sliderWrap.style.transform = `translateX(-${(counter - 1) * imgLengh}px)`;
+    }
+    else {
+        counter = 0;
+        sliderWrap.style.transform = `translateX(${(counter + 1) * imgLengh}px)`;
+    }
+
+    sliderPoints.forEach(point => {
+        point.style.opacity = "20%";
+    });
+
+    sliderPoints[counter].style.opacity = "100%";
+});
+
+leftBtn.addEventListener("click", () => {
+    if (counter != 0) {
+        counter--;
+        sliderWrap.style.transform = `translateX(${-(counter - 1) * imgLengh}px)`;
+    }
+    else {
+        counter = sliderImgs.length - 1;
+        sliderWrap.style.transform = `translateX(-${(counter - 1) * imgLengh}px)`;
+    }
+
+    sliderPoints.forEach(point => {
+        point.style.opacity = "20%";
+    });
+
+    sliderPoints[counter].style.opacity = "100%";
 });
