7bc312fc1e5c4006cc071c595e6145fe9dd55a3d
Nicu2002 <nicu.mercurial@bk.ru>
2022-03-14 18:35:55 +0200

    finished search-box

--- FourWheels/Scripts/search-bar.js
+++ FourWheels/Scripts/search-bar.js
@@ -1,22 +1,34 @@
 'use strict';
 
-// const carsCards = document.querySelectorAll(".car-card");
-// const carsTitles = carsCards.querySelectorAll(".card-title");
+const carsCards = document.querySelectorAll(".car-card");
+const carsTitles = document.querySelectorAll(".card-title");
 
 const inputField = document.querySelector("#search-bar");
 const cards = document.querySelectorAll(".car-card");
 
 let inputValue;
+let resultsCounter;
+
+const numberOfResults = document.querySelector("#sort-menu p");
 
 inputField.addEventListener("keypress", (event) => {
     if (event.keyCode === 13) {
-        event.preventDefault();
         inputValue = inputField.value;
-        // filterCards();
+        event.preventDefault();
+        filterCards(inputValue);
     }
 });
 
 function filterCards(inputValue) {
-    
+    if (inputValue != "") {
+        resultsCounter = 0;
+        carsCards.forEach((el, i) => {
+            carsCards[i].style.display = "none";
+            if (el.children[1] && el.children[1].textContent.toLowerCase().includes(inputValue.toLowerCase()) == true) {
+                carsCards[i].style.display = "block";
+                resultsCounter++;
+            }
+        });
+        numberOfResults.innerHTML = `(${resultsCounter})`;
+    }
 }
-
